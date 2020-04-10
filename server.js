8703d2698d5f6669f20438ab2e3f740d44b4ca19
Brendon Hampton <brendonhampton1988@gmail.com>
2020-04-09 20:38:07 -0700

    got the server file fixed

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 'use strict';
 
 require('dotenv').config();
+
+const cors = require('cors');
 const express = require('express');
 
-const app = express();
-app.listen(process.env.PORT() => console.log('server up on' PORT.));
+const PORT = process.env.PORT;
 
+const app = express();
+
+app.use(cors());
 console.log(process.env.PORT);
+
+app.get( '/test', (request, response) => {
+  const name = request.query.name;
+  response.send(`this is ${name}`);
+});
+
+app.listen(PORT, () => console.log('server up on', PORT));
