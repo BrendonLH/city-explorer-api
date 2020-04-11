207a3b7f95646c6f63bb0dd71540388691225c77
Brendon Hampton <brendonhampton1988@gmail.com>
2020-04-10 20:38:20 -0700

    added weather and finished location

--- server.js
+++ server.js
@@ -17,21 +17,41 @@ app.get( '/test', (request, response) => {
   response.send(`this is ${name}`);
 });
 
+app.get('/location', handleLocation);
+
+
 function handleLocation( request, response) {
   let city = request.query.city;
   let fixedData = require('./data/geo.json');
   let location = new Location(city,fixedData[0]);
-  console.log(location);
+  //   console.log(location);
   response.json(location);
 }
 
 function Location(city, data) {
   this.search_query = city;
-  this.formattedQuery = data.display_name;
-  this.lattitude = data.lat;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
   this.longitude = data.lon;
 }
 
+app.get('/weather', handleWeather);
+
+function handleWeather( request, response) {
+  let weatherList = [];
+  let weatherData = require('./data/darksky.json');
+  weatherData.daily.data.forEach(w => {
+    let weather = new Weather(w);
+    weatherList.push(weather);
+    console.log(w);
+  });
+  response.json(weatherList);
+}
+
+function Weather(data) {
+  this.forecast = data.summary;
+  this.time = data.time;
+}
 
 
 app.listen(PORT, () => console.log('server up on', PORT));
